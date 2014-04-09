bb62dcefd62b705f45d2db910548f83d415f05e8
mangeshphadtare <get2mangesh@gmail.com>
2014-04-09 18:41:41 +0530

    last commit

--- src/test/java/com/mangesh/olpay/auth/UserManagementTest.java
+++ src/test/java/com/mangesh/olpay/auth/UserManagementTest.java
@@ -1,5 +1,6 @@
 package com.mangesh.olpay.auth;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -9,12 +10,28 @@ public class UserManagementTest {
 	@Test
 	public void authUserByPhoneNumber() {
 
+		//given
 		User u1 = new User("Tushar","9225866145","password");
 		User u2 = new User("rahul","1234567890","password");
 		User u3 = new User("rajesh","0123456789","password");
 		User u4 = new User("sumedh","9012345678","password");
 		
 		
+		UserCollection usercollection = new usercollection(new BookMySQLStoreTestDouble());
+		UserData userdata = new UserData(usercollection);
+		UserManagement usermanagement = new UserManagement(usercollection);
+		
+		//when
+		UserData.addUser(u1);
+		UserData.addUser(u2);
+		UserData.addUser(u3);
+		UserData.addUser(u4);
+		
+		//then
+		User actual = UserManagement.getUserByPhoneNumber("9225866145");
+		
+		assertEquals(u1, actual);
+
 		
 	}
 
