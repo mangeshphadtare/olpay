a23f4d523286c0c5b223961fc464388f249659c4
mangeshphadtare <get2mangesh@gmail.com>
2014-04-09 18:22:44 +0530

    commited 9april 6:23

---
+++ src/test/java/com/mangesh/olpay/auth/UserManagementTest.java
@@ -0,0 +1,21 @@
+package com.mangesh.olpay.auth;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserManagementTest {
+
+	@Test
+	public void authUserByPhoneNumber() {
+
+		User u1 = new User("Tushar","9225866145","password");
+		User u2 = new User("rahul","1234567890","password");
+		User u3 = new User("rajesh","0123456789","password");
+		User u4 = new User("sumedh","9012345678","password");
+		
+		
+		
+	}
+
+}
